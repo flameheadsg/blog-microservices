859092a756ad0eb3cbaac9e0d32aad80e6095093
Sunny Gupta <flameheadsg@gmail.com>
2020-06-12 16:49:00 -0700

    conditionally render comments based on status

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -2,6 +2,17 @@ import React from 'react';
 
 export default ({ comments }) => {
   const renderedComments = Object.values(comments).map(comment => {
+    switch (comment.status) {
+      case 'PENDING':
+        comment.comment = 'This comment is pending approval...';
+        break;
+      case 'REJECTED':
+        comment.comment = 'This comment has been rejected by moderator.';
+        break;
+      case 'APPROVED':
+      default:
+    }
+    
     return (
       <li
         key={comment.id}
