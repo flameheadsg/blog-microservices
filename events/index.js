8cba836170a6a4a17e502351064541f92dd926dd
Sunny Gupta <flameheadsg@gmail.com>
2020-06-12 16:58:35 -0700

    added event queue for query service to pull from during server initialization

--- events/index.js
+++ events/index.js
@@ -5,15 +5,23 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 // endpoint publishes events to various services
 app.post('/events', async (req, res) => {
   console.log('received event:', req.body.type);
   const event = req.body;
+  events.push(event);
+
   await axios.post('http://localhost:4000/events', event); // posts
   await axios.post('http://localhost:4001/events', event); // comments
   await axios.post('http://localhost:4002/events', event); // query
   await axios.post('http://localhost:4003/events', event); // moderator
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
